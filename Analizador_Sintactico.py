a940761266e89e38c86546c019796859ec2c1129
Luis Esteban <luis_esteban_95@hotmail.es>
2021-02-21 19:11:35 +0100

    Ultimos detalles para extraer resultados

--- Analizador_Sintactico.py
+++ Analizador_Sintactico.py
@@ -7,13 +7,25 @@ from nltk import load_parser
 cp = load_parser('gramatica_base.fcfg')
 infile = open('materialpracticasintaxis/textos.txt', encoding='utf-8')
 # Mostramos por pantalla lo que leemos desde el fichero
+# Si pinta es verdader pinta el arbol
+pinta = False
+with open('resutado.txt', 'w') as f:
+    f.write('')
 for i, line in enumerate(infile):
     print(line)
+    with open('resutado.txt', 'a') as f:
+        f.write(line)
     tokens=line.split()
     trees = cp.parse(tokens)
     for tree in trees:
         print(tree)
-        print("="*10+"OK"+"="*10)
-        # tree.draw()
+        with open('resutado.txt', 'a') as f:
+            f.write(tree.pformat()+"\n")
+        if pinta:
+            tree.draw()
+    with open('resutado.txt', 'a') as f:
+        f.write("\n")
+        f.write("=" * 50)
+        f.write("\n")
 infile.close()
 
